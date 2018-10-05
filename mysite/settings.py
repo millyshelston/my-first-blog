002e4a98b565e5c30fc6f07c8d7d8c86dd8b9f3a
milly Shelston <emiliashelston@hotmail.com>
2018-10-05 13:04:09 +1000

    Modified templates to display posts from database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ye4*hw*gxb7xv)o_*=bzygn$6gosz^#cu(86mks6t$h2jzl76^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -120,3 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
