3cfdccaf13818eafba87de29116522fb2085fe95
milly Shelston <emiliashelston@hotmail.com>
2018-10-18 14:12:27 +1100

    p

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,9 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
+def blog1(request):
+    return render(request, 'blog/blog1.html')
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
